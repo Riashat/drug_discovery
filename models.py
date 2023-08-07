1818acad6a6755a7ff62850584b99543da3b42cc
Riashat Islam <Riashat@tasnovas-mbp.myfiosgateway.com>
2023-08-06 23:20:53 -0400

    models and helpers

---
+++ models.py
@@ -0,0 +1,376 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+#@title Importing RDKit for molecule parsing
+import rdkit as rdkit
+import rdkit.Chem as Chem
+from rdkit.Chem import Draw
+from deepchem.feat import MolGraphConvFeaturizer as MGCF
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, Dropout
+from torch_geometric.nn import GCNConv, GATv2Conv
+import math
+import torch
+import torch.nn as nn
+import random
+import argparse
+import os
+import pandas as pd
+import pickle
+
+from sklearn.metrics import roc_curve, roc_auc_score
+
+
+
+'''
+---------
+
+ATTENTION AND TRANSFORMER MODELS
+
+---------
+'''
+
+from torch import nn, einsum
+from einops import rearrange
+
+### Model Definition
+
+# constants
+DIST_KERNELS = {
+    'exp': {
+        'fn': lambda t: torch.exp(-t),
+        'mask_value_fn': lambda t: torch.finfo(t.dtype).max
+    },
+    'softmax': {
+        'fn': lambda t: torch.softmax(t, dim = -1),
+        'mask_value_fn': lambda t: -torch.finfo(t.dtype).max
+    }
+}
+
+# helpers
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return d if not exists(val) else val
+
+# helper classes
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return x + self.fn(x, **kwargs)
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        x = self.norm(x)
+        return self.fn(x, **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, dim_out = None, mult = 4):
+        super().__init__()
+        dim_out = default(dim_out, dim)
+        self.net = nn.Sequential(
+            nn.Linear(dim, dim * mult),
+            nn.GELU(),
+            nn.Linear(dim * mult, dim_out)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, Lg = 0.5, Ld = 0.5, La = 1, dist_kernel_fn = 'exp'):
+        super().__init__()
+        inner_dim = dim_head * heads
+        self.heads= heads
+        self.scale = dim_head ** -0.5
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+
+        # hyperparameters controlling the weighted linear combination from
+        # self-attention (La)
+        # adjacency graph (Lg)
+        # pair-wise distance matrix (Ld)
+
+        self.La = La
+        self.Ld = Ld
+        self.Lg = Lg
+
+        self.dist_kernel_fn = dist_kernel_fn
+
+    def forward(self, x, mask = None, adjacency_mat = None, distance_mat = None):
+        h, La, Ld, Lg, dist_kernel_fn = self.heads, self.La, self.Ld, self.Lg, self.dist_kernel_fn
+
+        qkv = self.to_qkv(x)
+        q, k, v = rearrange(qkv, 'b n (h qkv d) -> b h n qkv d', h = h, qkv = 3).unbind(dim = -2)
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        assert dist_kernel_fn in DIST_KERNELS, f'distance kernel function needs to be one of {DISTANCE_KERNELS.keys()}'
+        dist_kernel_config = DIST_KERNELS[dist_kernel_fn]
+
+        if exists(distance_mat):
+            distance_mat = rearrange(distance_mat, 'b i j -> b () i j')
+
+        if exists(adjacency_mat):
+            adjacency_mat = rearrange(adjacency_mat, 'b i j -> b () i j')
+
+        if exists(mask):
+            mask_value = torch.finfo(dots.dtype).max
+            mask = mask[:, None, :, None] * mask[:, None, None, :]
+
+            # mask attention
+            dots.masked_fill_(~mask, -mask_value)
+
+            if exists(distance_mat):
+                # mask distance to infinity
+                # todo - make sure for softmax distance kernel, use -infinity
+                dist_mask_value = dist_kernel_config['mask_value_fn'](dots)
+                distance_mat.masked_fill_(~mask, dist_mask_value)
+
+            if exists(adjacency_mat):
+                adjacency_mat.masked_fill_(~mask, 0.)
+
+        attn = dots.softmax(dim = -1)
+
+        # sum contributions from adjacency and distance tensors
+        attn = attn * La
+
+        if exists(adjacency_mat):
+            attn = attn + Lg * adjacency_mat
+
+        if exists(distance_mat):
+            distance_mat = dist_kernel_config['fn'](distance_mat)
+            attn = attn + Ld * distance_mat
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+# main class
+class AttenModel(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim_in,
+        model_dim,
+        dim_out,
+        depth,
+        heads = 8,
+        Lg = 0.5,
+        Ld = 0.5,
+        La = 1,
+        dist_kernel_fn = 'exp'
+    ):
+        super().__init__()
+
+        self.embed_to_model = nn.Linear(dim_in, model_dim)
+        self.layers = nn.ModuleList([])
+        self.attention = Attention(model_dim, heads = heads, Lg = Lg, Ld = Ld, La = La, dist_kernel_fn = dist_kernel_fn)
+
+        for _ in range(depth):
+            layer = nn.ModuleList([
+                Residual(PreNorm(model_dim, self.attention)),
+                Residual(PreNorm(model_dim, FeedForward(model_dim)))
+            ])
+            self.layers.append(layer)
+
+        self.norm_out = nn.LayerNorm(model_dim)
+        self.ff_out = FeedForward(model_dim, dim_out)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(
+        self,
+        x,
+        mask = None,
+        adjacency_mat = None,
+        distance_mat = None
+    ):
+        x = self.embed_to_model(x)
+
+        for (attn, ff) in self.layers:
+            x = attn(
+                x,
+                mask = mask,
+                adjacency_mat = adjacency_mat,
+                distance_mat = distance_mat
+            )
+            x = ff(x)
+
+        x = self.norm_out(x)
+        x = x.mean(dim = -2)
+        x = self.ff_out(x)
+        x = self.sigmoid(x)
+        return x
+
+
+
+'''
+---------
+
+GRAPH NETWORK RELATED MODELS
+
+---------
+'''
+class GraphAttentionLayer(nn.Module):
+    def __init__(self, in_features, out_features, dropout):
+        super(GraphAttentionLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.dropout = dropout
+        # Weight
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        self.weight2 = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        stdv = 1. / math.sqrt(self.weight2.size(1))
+        self.weight2.data.uniform_(-stdv, stdv)
+                        
+    def forward(self, x, adj):
+        batch_size = 1 
+        x = x.reshape(batch_size, x.shape[0], x.shape[1])
+        node_count = x.size()[1]
+        x = x.reshape(batch_size * node_count, x.size()[2])
+        x = torch.mm(x, self.weight)
+        x = x.reshape(batch_size, node_count, self.weight.size()[-1])
+        # Attention score
+        attention_input = torch.cat([x.repeat(1, 1, node_count).view(batch_size, node_count * node_count, -1), x.repeat(1, node_count, 1)], dim=2).view(batch_size, node_count, -1, 2 * self.out_features)
+        e = F.relu(torch.matmul(attention_input, self.weight2).squeeze(3))
+        zero_vec = -9e15*torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = F.softmax(attention, dim=2)
+        attention = F.dropout(attention, self.dropout, training=self.training)        
+        ## input 1 : bxnxm
+        ## input 2 : bxmxp
+        ##output : bxnxp
+        x = torch.bmm(attention, x) 
+        return x
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+
+
+
+
+class GAT(torch.nn.Module):
+    """Graph Attention Network"""
+    def __init__(self, dim_in, dim_h, dim_out, heads=8):
+        super().__init__()
+
+        self.gat1 = GraphAttentionLayer(dim_in, dim_h, dropout=0.1)
+        self.gat2 = GraphAttentionLayer(dim_in, dim_h, dropout=0.1)
+        self.fc1 = nn.Linear(128, 128)
+        self.hc = torch.nn.Linear(128, dim_out)
+        self.optimizer = torch.optim.Adam(self.parameters(),
+                                            lr=0.005,
+                                            weight_decay=5e-4)
+        self.sigmoid = torch.nn.Sigmoid()
+        self.softmax = torch.softmax
+                
+    def forward_attention(self, x, adj_mat):
+        h = F.dropout(x, p=0.6, training=self.training)
+        h = self.gat1(x, adj_mat)
+        h = self.gat2(x, adj_mat)
+        h = torch.max(h, dim=1)[0].squeeze() # max readout
+        h = F.relu(h)
+        h = F.dropout(h, p=0.6, training=self.training)
+        x = F.relu(self.fc1(h))
+        x = self.hc(x)
+        x = x.reshape(1, x.shape[0])
+        out = F.log_softmax(x, dim=1)
+        softmax = torch.exp(out) 
+        prob = list(softmax.detach().numpy())
+        predictions = np.argmax(prob, axis=1)
+        return x, out, predictions
+
+    def forward(self, x, edge_index):
+        x, out, pred = self.forward_attention(x, edge_index)
+        return x, out, pred
+
+
+class GCN(torch.nn.Module):
+  """Graph Convolutional Network"""
+  def __init__(self, dim_in, dim_h, dim_out):
+    super().__init__()
+    self.gcn1 = GCNConv(dim_in, dim_h)
+    self.gcn2 = GCNConv(dim_h, dim_out)
+    self.optimizer = torch.optim.Adam(self.parameters(),
+                                      lr=0.001,
+                                      weight_decay=5e-4)
+    self.sigmoid = torch.nn.Sigmoid()
+    self.softmax = torch.softmax
+
+  def forward(self, x, edge_index):
+    h = F.dropout(x, p=0.5, training=self.training)
+    h = self.gcn1(h, edge_index)
+    h = torch.relu(h)
+    h = F.dropout(h, p=0.5, training=self.training)
+    h = self.gcn2(h, edge_index)
+    out = F.log_softmax(h, dim=1)
+    softmax = torch.exp(out) #output from linear
+    prob = list(softmax.detach().numpy())
+    predictions = np.argmax(prob, axis=1)
+
+    return h, out, predictions
+
+
+
+class GCN_GAT(torch.nn.Module):
+  """Graph Attention Network"""
+  def __init__(self, dim_in, dim_h, dim_out, heads=8):
+    super().__init__()
+    self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
+    self.gat2 = GATv2Conv(dim_h*heads, dim_out, heads=1)
+    self.optimizer = torch.optim.Adam(self.parameters(),
+                                      lr=0.005,
+                                      weight_decay=5e-4)
+    self.sigmoid = torch.nn.Sigmoid()
+    self.softmax = torch.softmax
+
+  def forward(self, x, edge_index):
+    h = F.dropout(x, p=0.6, training=self.training)
+    h = self.gat1(x, edge_index)
+    h = F.elu(h)
+    h = F.dropout(h, p=0.6, training=self.training)
+    h = self.gat2(h, edge_index)
+
+    out = F.log_softmax(h, dim=1)
+    softmax = torch.exp(out) #output from linear
+    prob = list(softmax.detach().numpy())
+    predictions = np.argmax(prob, axis=1)
+    
+    return h, out, predictions
+
+
+
+class GraphMLP(torch.nn.Module):
+    def __init__(self, features, num_classes=2):
+        super().__init__()
+        self.fc1 = nn.Linear(features, 64)
+        self.fc2 = nn.Linear(64, num_classes)
+
+        self.softmax = torch.softmax
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.005, weight_decay=5e-4)
+
+    def forward(self, features, edge_index):
+        input = self.fc1(features)
+        input = F.relu(input)
+        input = self.fc2(input)
+        out = F.log_softmax(input, dim=1)
+
+        softmax = torch.exp(out) #output from linear
+        prob = list(softmax.detach().numpy())
+        predictions = np.argmax(prob, axis=1)
+    
+        return input, out, predictions
---
+++ utils.py
@@ -0,0 +1,299 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+#@title Importing RDKit for molecule parsing
+import rdkit as rdkit
+import rdkit.Chem as Chem
+from rdkit.Chem import Draw
+from deepchem.feat import MolGraphConvFeaturizer as MGCF
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, Dropout
+from torch_geometric.nn import GCNConv, GATv2Conv
+import math
+import torch
+import torch.nn as nn
+import random
+import argparse
+import os
+import pandas as pd
+import pickle
+
+def train_val_test(X_smiles, Y):
+
+	# feature_path = 'data_features.pkl'
+	# if not os.path.exists(feature_path): 
+	#     X_all, Y_all = load_data_from_smiles(x_smiles=X_smiles, labels= Y, normalize_features=False)
+	#     print('Saving molecule features transformed from raw smiles')
+	#     pickle.dump((X_all, Y_all), open(feature_path, 'wb'))
+	# else:
+	#     X_all, Y_all = pickle.load(open(feature_path, "rb"))
+
+	train_idx = [np.random.randint(len(X_smiles)) for _ in range(int(0.6*len(X_smiles)))]
+
+	all_idx = [i for i in range(len(X_smiles))]
+	val_idx = [i for i in all_idx if i not in train_idx]
+
+	train_dataset = []
+	train_y = []
+	for i in train_idx:
+		train_dataset.append(X_smiles[i])
+		train_y.append(Y[i])
+	
+	val_dataset = []
+	val_y = []
+	for j in val_idx : 
+		val_dataset.append(X_smiles[j])
+		val_y.append(Y[j])
+
+	return train_dataset, train_y, val_dataset, val_y
+
+def load_data(active=False):
+
+    if not active:
+        data = 	pd.read_csv("/Users/Riashat/Documents/interviews/valence/drug_discovery/egfr_compounds_classification.csv",
+            index_col=0,
+        )
+        print("Number of active compounds:", int(data.active.sum()))
+        print("Number of inactive compounds:", len(data) - int(data.active.sum()))
+
+        #@title Load data / split to train and test
+        with open('/Users/Riashat/Documents/interviews/valence/drug_discovery/egfr_compounds_classification.csv') as fh:
+            smiles = []
+            logDs = []
+            header = True
+            for row in csv.reader(fh):
+                if header:
+                    header = False
+                    continue
+                smiles.append(row[2])  
+                logDs.append(float(row[4]))
+
+        train_dataset, train_y, val_dataset, val_y = train_val_test(smiles, logDs)
+
+    else:
+        data = 	pd.read_csv("/Users/Riashat/Documents/interviews/valence/drug_discovery/egfr_compounds_classification.csv",
+            index_col=0,
+        )
+        active_indices = np.load("active_indices.npy")
+        data = data.iloc[active_indices, :]
+
+        print("Number of active compounds:", int(data.active.sum()))
+        print("Number of inactive compounds:", len(data) - int(data.active.sum()))
+
+        #@title Load data / split to train and test
+        with open('/Users/Riashat/Documents/interviews/valence/drug_discovery/egfr_compounds_classification.csv') as fh:
+            smiles = []
+            logDs = []
+            header = True
+            for row in csv.reader(fh):
+                if header:
+                    header = False
+                    continue
+                smiles.append(row[2])  
+                logDs.append(float(row[4]))
+
+        train_dataset, train_y, val_dataset, val_y = train_val_test(smiles, logDs)
+        
+	
+    return train_dataset, train_y, val_dataset, val_y
+
+
+
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(y_pred)
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]
+    acc = torch.round(acc * 100)
+    
+    return acc
+
+
+
+def binary_acc2(y_pred, y_test):
+    y_pred_tag = torch.round(torch.Tensor(y_pred))
+    y_test = torch.Tensor(y_test)
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]
+    acc = torch.round(acc * 100)
+    
+    return acc
+
+
+
+class DotDict(dict):
+    """dot.notation access to dictionary attributes
+
+    Refer: https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary/23689767#23689767
+    """  # NOQA
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+
+def data_load():
+
+    data = pd.read_csv("/Users/Riashat/Documents/interviews/valence/drug_discovery/egfr_compounds.csv",
+        index_col=0,
+    )
+
+    print("Shape of dataframe : ", data.shape)
+    data.head()
+
+    # Keep only the columns we want
+    data = data[["molecule_chembl_id", "smiles", "pIC50"]]
+    # data = data.iloc[:1800, :]
+
+    data.head()
+    # NBVAL_CHECK_OUTPUT
+
+    data["active"] = np.zeros(len(data))
+
+    # Mark every molecule as active with an pIC50 of >= 6.3, 0 otherwise
+    data.loc[data[data.pIC50 > 8.0].index, "active"] = 1.0
+
+    # NBVAL_CHECK_OUTPUT
+    print("Number of active compounds:", int(data.active.sum()))
+    print("Number of inactive compounds:", len(data) - int(data.active.sum()))
+    # Number of active compounds: 873
+    # Number of inactive compounds: 3762
+
+    data.head()
+
+    data.to_csv("egfr_compounds_classification.csv")
+        
+    return data
+
+
+def draw_smiles(data):
+
+	# Get SMILES string
+	smiles = data["smiles"][0]
+	mol = Chem.MolFromSmiles(smiles) 
+
+	print(f"SMILES: {smiles}, Number of atoms: {len(mol.GetAtoms())}")
+	Draw.MolToImage(mol)
+
+
+
+def one_hot_vector(val, lst):
+    """Converts a value to a one-hot vector based on options in lst"""
+    if val not in lst:
+        val = lst[-1]
+    return map(lambda x: x == val, lst)
+
+
+def feature_normalize(x_all):
+    """Min Max Feature Scalling for Atom Feature Matrix"""
+    min_vec, max_vec = x_all[0][0].min(axis=0), x_all[0][0].max(axis=0)
+    for x in x_all:
+        min_vec = np.minimum(min_vec, x[0].min(axis=0))
+        max_vec = np.maximum(max_vec, x[0].max(axis=0))
+    diff = max_vec - min_vec
+    diff[diff == 0] = 1.
+
+    for x in x_all:
+        afm = x[0]
+        afm = (afm - min_vec) / diff
+        x[0] = afm
+
+    return x_all
+
+
+
+def generate_scaffold(smiles, include_chirality=False):
+    """
+    Obtain Bemis-Murcko scaffold from smiles
+    :param smiles:
+    :param include_chirality:
+    :return: smiles of scaffold
+    """
+    scaffold = MurckoScaffold.MurckoScaffoldSmiles(
+        smiles=smiles, includeChirality=include_chirality)
+    return scaffold
+
+def random_scaffold_split(smiles_list, frac_train=0.6, frac_valid=0.2, frac_test=0.2, seed=0):
+    """
+    Adapted from https://github.com/pfnet-research/chainer-chemistry/blob/master/chainer_chemistry/dataset/splitters/scaffold_splitter.py
+    Split dataset by Bemis-Murcko scaffolds
+    This function can also ignore examples containing null values for a
+    selected task when splitting. Deterministic split
+    :param smiles_list: list of smiles corresponding to the dataset obj
+    task_idx is provided
+    :param frac_train:
+    :param frac_valid:
+    :param frac_test:
+    :param seed;
+    :return: train, valid, test slices of the input dataset obj
+    """
+    np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
+
+    N = len(smiles_list)
+    rng = np.random.RandomState(seed)
+
+    scaffolds = defaultdict(list)
+    for ind, smiles in enumerate(smiles_list):
+        scaffold = generate_scaffold(smiles, include_chirality=True)
+        scaffolds[scaffold].append(ind)
+
+    scaffold_sets = rng.permutation(list(scaffolds.values()))
+
+    n_total_valid = int(np.floor(frac_valid * N))
+    n_total_test = int(np.floor(frac_test * N))
+
+    train_idx = []
+    valid_idx = []
+    test_idx = []
+
+    for scaffold_set in scaffold_sets:
+        if len(valid_idx) + len(scaffold_set) <= n_total_valid:
+            valid_idx.extend(scaffold_set)
+        elif len(test_idx) + len(scaffold_set) <= n_total_test:
+            test_idx.extend(scaffold_set)
+        else:
+            train_idx.extend(scaffold_set)
+
+    return train_idx, valid_idx, test_idx
+
+
+
+def pad_array(array, shape, dtype=np.float32):
+    """Pad a 2-dimensional array with zeros.
+
+    Args:
+        array (ndarray): A 2-dimensional array to be padded.
+        shape (tuple[int]): The desired shape of the padded array.
+        dtype (data-type): The desired data-type for the array.
+
+    Returns:
+        A 2-dimensional array of the given shape padded with zeros.
+    """
+    padded_array = np.zeros(shape, dtype=dtype)
+    padded_array[:array.shape[0], :array.shape[1]] = array
+    return padded_array
+
+
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+
+
+
+
